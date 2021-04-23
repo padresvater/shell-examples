f9bd3bd76fdaffba344eda12d5a6e38af6ddc9be
padresvater <1649299415@qq.com>
2021-04-23 08:35:17 +0000

    ph-processing finish

--- image_processing.sh
+++ image_processing.sh
@@ -17,22 +17,21 @@ then
         
     -h,--help                  显示本脚本的帮助文档
 
-    -q,--quality               对图片进行质量压缩时的质量参数，质量为1（最低图像质量和最高压缩率）
+    -com,--compress            对图片进行质量压缩时的质量参数，质量为1（最低图像质量和最高压缩率）
                                到100（最佳质量但有效压缩率最低）。
                                默认值是如果可以确定，则使用输入图像的估计质量，否则使用92。
                                如果质量大于90，则不会对色度通道进行下采样。
 
-    -r,--resize                保持原始宽高比的前提下压缩分辨率的比率
+    -res,--resize              保持原始宽高比的前提下压缩分辨率的比率
 
     -w,--watermark             需要在图片中添加的文本水印
 
-    -r,--rename                重命名图片,作为前缀添加到文件名中而不改变拓展名。
+    -ren,--rename              重命名图片,作为前缀添加到文件名中而不改变拓展名。
 
-    -c,--convert               将png和svg图片转化为jpg格式，不改变原有文件名。"
+    -con,--convert             将png和svg图片转化为jpg格式，不改变原有文件名。"
     exit 0
 fi
-echo "脚本传入参数$1"
-echo "================================"
+
 
 # 各函数变量名的初始化
 # 可修改，类似全局变量
@@ -42,6 +41,11 @@ watermark="padresvater"
 name="re_"
 dir="./image" 
 out="./OutPut"
+if_com=0
+if_res=0
+if_aw=0
+if_ren=0
+if_con=0
 
 # 图片的质量压缩函数
 function jpegcompress() {
@@ -101,5 +105,47 @@ function convert_to_jpg() {
     echo "==========转换格式完成=========="
     return
 }
- convert_to_jpg
+# convert_to_jpg
 
+# 以下实现命令行参数方式使用不同功能
+while true;do
+    case "$1" in
+        -com|--compress) 
+            if_com=1; shift ;;               
+        -res|--resize)
+            if_res=1; shift ;;
+        -w|--watermark)
+            if_aw=1; shift ;;
+        -ren|--rename)
+            if_ren=1; shift ;;
+        -con|--convert) 
+            if_con=1; shift ;;    
+         "") break;;
+    esac
+done
+
+# 主函数与输出重定向
+log=log.txt
+echo "程序开始运行" >$log 
+echo "if_com=$if_com">>$log
+echo "if_res=$if_res">>$log
+echo "if_aw=$if_aw">>$log
+echo "if_ren=$if_ren">>$log
+echo "if_con=$if_con">>$log
+echo "以下为shellcheck检查结果">>$log
+shellcheck "$0">>$log  
+if [[ $if_com == 1 ]];then
+    jpegcompress 2>>$log #2即标准错误输入文件
+fi
+if [[ $if_res == 1 ]];then
+    resolution 2>>$log
+fi
+if [[ $if_aw == 1 ]];then
+    addwatermark 2>>$log
+fi
+if [[ $if_ren == 1 ]];then
+    rename 2>>$log
+fi
+if [[ $if_con == 1 ]];then
+    convert_to_jpg 2>>$log
+fi
